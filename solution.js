7b44811ea242274d3e77d1db82fb6f7be74f385b
davebro1031 <dwbroaddus@gmail.com>
2024-05-30 22:52:44 -0600

    add driver code with errors for bad target courses

--- solution.js
+++ solution.js
@@ -5,34 +5,54 @@ const tests = require("./data/tests.json")
 
 // helper function to get prereqs for a given target course
 const getPrereqs = (target) => {
-
+    if (!Object.keys(catalog).includes(target)) {
+        throw new Error('Target course not found')
+    }
+    return catalog[target]
 }
 
-const prereqsSatisfied = (takenCourses, targetPrereqs) => {
+// recursive helper function to determine whether prereqs have been met, inputs are:
+// completedCourses: the courses an individual has already completed
+const prereqsSatisfied = (completedCourses, targetPrereqs) => {
 
-    switch (targetPrereqs['type']){
+    switch (targetPrereqs['type']) {
         // Base case:  if we reach the 'bottom' level of the prereqs, 
         // check if that class is in our taken courses array
         case 'course':
-            return takenCourses.includes(targetPrereqs['code'])
+            return completedCourses.includes(targetPrereqs['code'])
 
         // Otherwise, use the appropriate logic on the next level
         case 'and':
             // begin true and then if any operands are false, return false
-            return targetPrereqs['operands'].reduce((acc, curr) => acc && prereqsSatisfied(takenCourses, curr), true)
-        
+            return targetPrereqs['operands'].reduce((acc, curr) => acc && prereqsSatisfied(completedCourses, curr), true)
+
         case 'or':
             // begin false, then if any operands are true, return true
-            return targetPrereqs['operands'].reduce((acc, curr) => acc || prereqsSatisfied(takenCourses, curr), false)
+            return targetPrereqs['operands'].reduce((acc, curr) => acc || prereqsSatisfied(completedCourses, curr), false)
     }
 }
 
-// const i = 1
-// const testTarget = tests[i]['course']
-// const testTaken = tests[i]['completedCourses']
-const testTarget = "ECON 300"
-const testTaken = ["ECON 2110", "ECON 2120", "MATH 1512"]
-const targetPrereqs = catalog[testTarget]
 
-console.log(prereqsSatisfied(testTaken, targetPrereqs))
+
+
+
+const testTarget = "ECON 300"
+const testCompleted = ["ECON 2110", "ECON 2120", "MATH 1512"]
+
+const analysis = (target, completed) => {
+    try{
+        const targetPrereqs = getPrereqs(target)
+
+        const result = prereqsSatisfied(completed, targetPrereqs)
+
+        console.log(result)
+
+    } catch (e) {
+        console.error(e)
+    }
+
+    
+}
+
+analysis(testTarget, testCompleted)
 
