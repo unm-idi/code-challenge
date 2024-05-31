b3745184ec7999e4163c655392ae0140695e1d51
davebro1031 <dwbroaddus@gmail.com>
2024-05-31 09:27:28 -0600

    remove unnecessary part 1 solution

--- solution.js
+++ solution.js
@@ -11,55 +11,6 @@ const getPrereqs = (target) => {
     return catalog[target]
 }
 
-// recursive helper function to determine whether prereqs have been met, inputs are:
-// completedCourses: the courses an individual has already completed
-const prereqsSatisfied = (completedCourses, targetPrereqs) => {
-
-    if(!targetPrereqs) return true
-
-    const type = targetPrereqs['type']
-
-    // Base case:  if we reach the 'bottom' level of the prereqs, 
-    // check if that class is in our taken courses array
-    if (type === 'course') {
-        return completedCourses.includes(targetPrereqs['code'])
-
-    // Otherwise, use the appropriate logic on the next level
-    } else {
-        const boolops = {
-            'and': (a, b) => a && b,
-            'or': (a, b) => a || b
-        }
-
-        // if we are using the "and" operator, then we begin true 
-        // then if any operands are false, return false
-        // if we are using the "or" operator, we do the opposite
-        const start = (type === "and")
-
-        return targetPrereqs['operands'].reduce((acc, curr) =>
-            boolops[type](acc, prereqsSatisfied(completedCourses, curr)),
-            start)
-    }
-}
-
-
-
-
-
-const analysis = (target, completed) => {
-    try {
-        const targetPrereqs = getPrereqs(target)
-
-        const result = prereqsSatisfied(completed, targetPrereqs)
-
-        // console.log(result)
-        return result
-
-    } catch (e) {
-        console.error(e)
-    }
-
-}
 
 const findFewestAdditionalCourses = (targetPrereqs, completed, memo) => {
     
@@ -135,25 +86,19 @@ const solve = (target, completed) => {
 
 }
 
-// const testTarget = "MATH 1512"
-// const testCompleted = ["MATH 1215"]
-// const testPrereqs = getPrereqs(testTarget)
-// const result = findFewestAdditionalCourses(testPrereqs, testCompleted, {})
-// console.log(result)
-
 for(let i = 0; i < tests.length; i++){
     let fewestAdditionalCoursesNeeded = solve(tests[i]['course'], tests[i]['completedCourses'])
     tests[i]['isSatisfied'] = (fewestAdditionalCoursesNeeded.length === 0)
     tests[i]['coursesNeeded'] = fewestAdditionalCoursesNeeded
 }
 
-console.log(tests)
-// fs.writeFile(
-//     "output.json",
-//     JSON.stringify(tests),
-//     err => {
-//         if (err) throw err
-//         console.log("Done writing")
-//     }
-// )
+
+fs.writeFile(
+    "output.json",
+    JSON.stringify(tests),
+    err => {
+        if (err) throw err
+        console.log("Done writing")
+    }
+)
 
